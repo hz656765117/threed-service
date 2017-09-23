df8212ed75ac9ce604682116e4efe57c4e67bac2
huangzhuo <656765117@qq.com>
2017-09-24 00:29:13 +0800

    zip解压方法修改

--- threed-service/src/main/java/com/hz/base/utils/ZipUtil.java
+++ threed-service/src/main/java/com/hz/base/utils/ZipUtil.java
@@ -6,7 +6,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
  
@@ -19,6 +23,8 @@ import java.util.zip.ZipOutputStream;
  *
  */
 public final class ZipUtil {
+	
+    private static String sepa = java.io.File.separator;
  
     private ZipUtil() {
         // empty
@@ -111,8 +117,12 @@ public final class ZipUtil {
             try {
                 zis = new ZipInputStream(new FileInputStream(source));
                 ZipEntry entry = null;
-                while ((entry = zis.getNextEntry()) != null
-                        && !entry.isDirectory()) {
+                
+//                System.out.println(zis.getNextEntry());
+//                entry = zis.getNextEntry();
+//                System.out.println(entry.isDirectory());
+                //&& !entry.isDirectory()
+                while ((entry = zis.getNextEntry()) != null ) {
                     File target = new File(source.getParent(), entry.getName());
                     if (!target.getParentFile().exists()) {
                         // 创建文件父目录
@@ -135,6 +145,65 @@ public final class ZipUtil {
             }
         }
     }
+    
+    
+    
+    
+    
+    
+    
+    /** 
+     * 解压到指定目录 
+     * @param zipPath 
+     * @param descDir 
+     * @author isea533 
+     */  
+    public static void unZipFiles(String zipPath,String descDir)throws IOException{  
+        unZipFiles(new File(zipPath), descDir);  
+    }  
+    /** 
+     * 解压文件到指定目录 
+     * @param zipFile 
+     * @param descDir 
+     * @author isea533 
+     */  
+    @SuppressWarnings("rawtypes")  
+    public static void unZipFiles(File zipFile,String descDir)throws IOException{  
+        File pathFile = new File(descDir);  
+        if(!pathFile.exists()){  
+            pathFile.mkdirs();  
+        }  
+        ZipFile zip = new ZipFile(zipFile);  
+        for(Enumeration entries = zip.entries();entries.hasMoreElements();){  
+            ZipEntry entry = (ZipEntry)entries.nextElement();  
+            String zipEntryName = entry.getName();  
+            InputStream in = zip.getInputStream(entry);  
+            String outPath =  descDir+ sepa +zipEntryName ;  
+            //判断路径是否存在,不存在则创建文件路径  
+            File file = new File(outPath.substring(0, outPath.lastIndexOf('/')));  
+            if(!file.exists()){  
+                file.mkdirs();  
+            }  
+            //判断文件全路径是否为文件夹,如果是上面已经上传,不需要解压  
+            if(new File(outPath).isDirectory()){  
+                continue;  
+            }  
+            //输出文件路径信息  
+            System.out.println(outPath);  
+              
+            OutputStream out = new FileOutputStream(outPath);  
+            byte[] buf1 = new byte[1024];  
+            int len;  
+            while((len=in.read(buf1))>0){  
+                out.write(buf1,0,len);  
+            }  
+            in.close();  
+            out.close();  
+            }  
+        System.out.println("******************解压完毕********************");  
+    }  
+    
+ 
  
  
 }
